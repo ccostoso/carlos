8b7e3202bf6fe20541bfcb4464ca262a808d2c66
Carlos Costoso <carlos.costoso@outlook.com>
2026-08-14 17:09:16 -0400

    Adjusts mobile layout

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,7 @@ export const projects: Project[] = [
     {
         name: "Pokémoves",
         description:
-            "What would a portfolio be without a PokéAPI-based app? Pokémoves makes comparing learnsets for Pokémon across generations easy. Should you wait to learn Hyper Voice before evolving Crocalor or take the stat boost and wait another four levels? At what level Pikachu learn Thunderbolt in Red and Blue\u2014or only in Yellow? Use Pokémoves to find out, and create an account to save your research!\n\nPokémoves is a full-stack app built with Next.js, Prisma 7, Postgres, Better Auth and ECS/Fargate. It uses the PokéAPI to fetch Pokémon data via GraphQL queries and caches results for fast lookups.",
+            "What would a portfolio be without a PokéAPI-based app? Pokémoves makes comparing learnsets for Pokémon across generations easy. Should you wait to learn Hyper Voice before evolving Crocalor or take the stat boost and wait another four levels? At what level does Pikachu learn Thunderbolt in Red and Blue... or did they only start learning it by leveling up in Yellow? Use Pokémoves to find out, and create an account to save your research!\n\nPokémoves is a full-stack app built with Next.js, Prisma 7, Postgres, Better Auth and ECS/Fargate. It uses the PokéAPI to fetch Pokémon data via GraphQL queries and caches results for fast lookups.",
         status: "active",
         badges: ["Prisma 7 · Postgres", "GraphQL", "Better Auth", "Next.js", "ECS · Fargate", "GitHub Actions"],
         link: "https://pokemoves.costo.so",
